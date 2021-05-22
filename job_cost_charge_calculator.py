82c1448184c4283faba11e8ad09b7ba8c8809fb8
caitlinnay <60642044+caitlinnay@users.noreply.github.com>
2021-05-23 02:20:11 +1200

    Update job_cost_charge_calculator.py
    Using the length of line rule

--- job_cost_charge_calculator.py
+++ job_cost_charge_calculator.py
@@ -29,65 +29,80 @@ class JobCost:
         self.logo_label.grid(row = 0, column = 0, columnspan = 4)
             
         #Add a Job Label
-        self.add_job_label = Label(self.add_job_frame, text = "Add a Job", font = ("Sans Serif", 17))
+        self.add_job_label = Label(self.add_job_frame, text = "Add a Job",
+                                   font = ("Sans Serif", 17))
         self.add_job_label.grid(row = 1, column = 0, sticky = NW)
 
         #Job Number Label
-        self.job_num_label = Label(self.add_job_frame, text = "Job Number", font = ("Sans Serif", 11))
+        self.job_num_label = Label(self.add_job_frame, text = "Job Number",
+                                   font = ("Sans Serif", 11))
         self.job_num_label.grid(row = 2, column = 0, sticky = NW)
 
         #Job Number Input
         self.job_num_var = IntVar()
         self.job_num_var.set("")
-        self.job_num_box = Entry(self.add_job_frame, font = ("Sans Serif", 10), textvariable = self.job_num_var, width = 30)
+        self.job_num_box = Entry(self.add_job_frame, font = ("Sans Serif", 10),
+                                 textvariable = self.job_num_var, width = 30)
         self.job_num_box.grid(row = 2, column = 1, columnspan = 3, sticky = NW)
 
         #Name Label
-        self.name_label = Label(self.add_job_frame, text = "Name", font = ("Sans Serif", 11))
+        self.name_label = Label(self.add_job_frame, text = "Name",
+                                font = ("Sans Serif", 11))
         self.name_label.grid(row = 3, column = 0, sticky = NW)
 
         #First Name Input
         self.first_name_var = StringVar()
         self.first_name_var.set("")
-        self.first_name_box = Entry(self.add_job_frame, font = ("Sans Serif", 10), width = 30, textvariable = self.first_name_var)
+        self.first_name_box = Entry(self.add_job_frame, font = ("Sans Serif", 10),
+                                    width = 30, textvariable = self.first_name_var)
         self.first_name_box.grid(row = 4, column = 0, sticky = NW)
 
         #Last Name Input
         self.last_name_var = StringVar()
         self.last_name_var.set("")
-        self.last_name_box = Entry(self.add_job_frame, font = ("Sans Serif", 10), width = 30, textvariable = self.last_name_var)
+        self.last_name_box = Entry(self.add_job_frame, font = ("Sans Serif", 10),
+                                   width = 30, textvariable = self.last_name_var)
         self.last_name_box.grid(row = 4, column = 1, columnspan = 3, sticky = NW)
 
         #First Name Label
-        self.first_name_label = Label(self.add_job_frame, text = "First Name", font = ("Sans Serif", 8))
+        self.first_name_label = Label(self.add_job_frame, text = "First Name",
+                                      font = ("Sans Serif", 8))
         self.first_name_label.grid(row = 5, column = 0, sticky = NW)
 
         #Last Name Label
-        self.last_name_label = Label(self.add_job_frame, text = "Last Name", font = ("Sans Serif", 8))
+        self.last_name_label = Label(self.add_job_frame, text = "Last Name",
+                                     font = ("Sans Serif", 8))
         self.last_name_label.grid(row = 5, column = 1, sticky = NW)
 
         #Distance Travelled Label
-        self.distance_label = Label(self.add_job_frame, text = "Distance Travelled to Client (km)", font = ("Sans Serif", 11))
+        self.distance_label = Label(self.add_job_frame,
+                                    text = "Distance Travelled to Client (km)",
+                                    font = ("Sans Serif", 11))
         self.distance_label.grid(row = 6, column = 0, sticky = NW)
 
         #Distance Input
         self.distance_var = StringVar() #String not Int as IntVar automatically rounds down
         self.distance_var.set("")
-        self.distance_box = Entry(self.add_job_frame, font = ("Sans Serif", 10), width = 30, textvariable = self.distance_var)
+        self.distance_box = Entry(self.add_job_frame, font = ("Sans Serif", 10),
+                                  width = 30, textvariable = self.distance_var)
         self.distance_box.grid(row = 6, column = 1, columnspan = 3, sticky = NW)
 
         #Minutes Spent on Virus Protection Label
-        self.minutes_label = Label(self.add_job_frame, text = "Minutes Spent on Virus Protection", font = ("Sans Serif", 11))
+        self.minutes_label = Label(self.add_job_frame,
+                                   text = "Minutes Spent on Virus Protection",
+                                   font = ("Sans Serif", 11))
         self.minutes_label.grid(row = 7, column = 0, sticky = NW)
 
         #Minutes Input
         self.minutes_var = IntVar()
         self.minutes_var.set("")
-        self.minutes_box = Entry(self.add_job_frame, font = ("Sans Serif", 10), width = 30, textvariable = self.minutes_var)
+        self.minutes_box = Entry(self.add_job_frame, font = ("Sans Serif", 10),
+                                 width = 30, textvariable = self.minutes_var)
         self.minutes_box.grid(row = 7, column = 1, columnspan = 3, sticky = NW)
 
         #Wof and tune Label
-        wof_and_tune_label = Label(self.add_job_frame, text = "Was a WOF and Tune Required?", font = ("Sans Serif", 11))
+        wof_and_tune_label = Label(self.add_job_frame, text = "Was a WOF and Tune Required?",
+                                   font = ("Sans Serif", 11))
         wof_and_tune_label.grid(row = 8, column = 0, sticky = NW)
 
         #Wof and tune radiobuttons
@@ -97,17 +112,22 @@ class JobCost:
         self.wof_tune_rbs = []
         
         for i in range(len(self.wof_tune_rbs_names)):
-            self.wof_tune_rbs.append(Radiobutton(self.add_job_frame, text = self.wof_tune_rbs_names[i], value = self.wof_tune_rbs_names[i],
-                                          variable = self.wof_tune_var, font = ("Sans Serif", 11)))
+            self.wof_tune_rbs.append(Radiobutton(self.add_job_frame,
+                                                 text = self.wof_tune_rbs_names[i],
+                                                 value = self.wof_tune_rbs_names[i],
+                                                  variable = self.wof_tune_var,
+                                                 font = ("Sans Serif", 11)))
             self.wof_tune_rbs[i].grid(row = 8, column = i +1, sticky = NW)
 
         #Enter Button to add a job
         #lambda code from https://www.reddit.com/r/learnpython/comments/cdfmn5/tkinter_help_command_running_before_clicked/
-        self.enter_job_btn = Button(self.add_job_frame, text = "Enter", font = ("Sans Serif", 11), width = 10, command = lambda:self.store_input()) 
+        self.enter_job_btn = Button(self.add_job_frame, text = "Enter", font = ("Sans Serif", 11),
+                                    width = 10, command = lambda:self.store_input()) 
         self.enter_job_btn.grid(row = 9, column = 3, sticky = NE)
 
         #Show Jobs Buttons
-        self.show_jobs_btn = Button(self.add_job_frame, text = "Show Jobs", font = ("Sans Serif", 11), width = 10, command = lambda:self.get_to_job_cards())
+        self.show_jobs_btn = Button(self.add_job_frame, text = "Show Jobs", font = ("Sans Serif", 11),
+                                    width = 10, command = lambda:self.get_to_job_cards())
         self.show_jobs_btn.grid(row = 10, column = 3, pady = 5, sticky = NE)
 
         self.job_cards_frame = Frame(parent)
@@ -130,6 +150,8 @@ class JobCost:
         self.job = [self.job_num, self.name, self.distance, self.minutes, self.wof_and_tune, self.charge]
         
         self.jobs.append(self.job)
+
+        print(self.jobs)
 
         #Reset Input Areas
         self.job_num_var.set("")
